f86e32ab5298d07bd0386e7bc16b95b929c049dc
Lea Cohen <cohenswim08@gmail.com>
2023-04-13 10:56:08 -0700

    Initial commit for gif svelte template repo

---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+    import "../styles/main.scss";
+</script>
+
+<h1>Welcome to the GIF SvelteKit Template Repo</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>Built with SvelteKit and Carbon Svelte Components</p>
