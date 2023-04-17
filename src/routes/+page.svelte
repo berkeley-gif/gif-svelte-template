b3eddc8b10eecb5ee95d1303c7fe78fe243bc553
Shruti Mukhtyar <mukhtyar@berkeley.edu>
2023-04-17 15:55:14 -0500

    add bar chart

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,19 @@
 <script>
-    import { Grid, Row, Column, CodeSnippet } from "carbon-components-svelte";
-    import { Button } from "carbon-components-svelte";
+    import { Grid, Row, Column, CodeSnippet, Button } from "carbon-components-svelte";
+    import { BarChartSimple } from "@carbon/charts-svelte";
+    import "@carbon/charts/styles.css";
   </script>
 
  <Grid>
 
-    <Row >
+    <Row>
         <Column>
             <h1>Welcome to the GIF SvelteKit Template</h1>
             <p>Built with SvelteKit and Carbon Svelte Components</p>
         </Column>
     </Row>
 
-    <Row>
+    <Row  style="margin-bottom: var(--cds-spacing-16)">
         <Column>
             <h2 class="h4">Sample buttons</h2>
             <Row>
@@ -27,6 +28,33 @@
                         <CodeSnippet type="inline" code="background-color: var(--cds-interactive-02)"  />
                     </div>
                     <Button kind="secondary">Secondary</Button>
+                </Column>
+            </Row>
+        </Column>
+    </Row>
+
+    <Row  style="margin-bottom: var(--cds-spacing-16)">
+        <Column>
+            <h2 class="h4">Sample line chart</h2>
+            <Row>
+                <Column sm={1} md={4}>
+                    <BarChartSimple
+  data={[
+    { group: "Qty", value: 65000 },
+    { group: "More", value: 29123 },
+    { group: "Sold", value: 35213 },
+    { group: "Restocking", value: 51213 },
+    { group: "Misc", value: 16932 },
+  ]}
+  options={{
+    title: "Simple bar (discrete)",
+    height: "400px",
+    axes: {
+      left: { mapsTo: "value" },
+      bottom: { mapsTo: "group", scaleType: "labels" },
+    },
+  }}
+/>
                 </Column>
             </Row>
         </Column>
