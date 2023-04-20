62be3cad34e4812c0d5b398ee6ed70e5c228ece7
Shruti Mukhtyar <mukhtyar@berkeley.edu>
2023-04-20 13:39:27 -0700

    remove carbon components from index page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,10 @@
-<script>
-	import { Grid, Row, Column, ClickableTile } from "carbon-components-svelte";
-</script>
+<h1>Welcome to the GIF SvelteKit Template</h1>
 
-<Grid>
-	<Row>
-		<Column>
-			<h1>Welcome to the GIF SvelteKit Template</h1>
-		</Column>
-	</Row>
-
-	<Row style="margin-bottom: var(--cds-spacing-16)">
-		<Column sm="{1}" md="{2}">
-			<ClickableTile href="/carbon">Carbon Components Svelte</ClickableTile>
-		</Column>
-		<Column sm="{1}" md="{2}">
-			<ClickableTile href="/material">Svelte Material UI</ClickableTile>
-		</Column>
-	</Row>
-</Grid>
+<ul>
+	<li>
+		<a href="/carbon">Using & Styling with Carbon Components Svelte</a>
+	</li>
+	<li>
+		<a href="/material">Using & Styling Svelte Material UI Components</a>
+	</li>
+</ul>
