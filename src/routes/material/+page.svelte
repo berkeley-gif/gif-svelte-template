b4e7ae7c1135fca8b8b608174cafa64d4d6d9674
Shruti Mukhtyar <mukhtyar@berkeley.edu>
2023-04-21 08:03:58 -0700

    add more material ui examples

--- src/routes/material/+page.svelte
+++ src/routes/material/+page.svelte
@@ -1,42 +1,70 @@
 <script>
-	import { Grid, Row, Column } from "carbon-components-svelte";
+	import LayoutGrid, { Cell as LayoutCell } from "@smui/layout-grid";
 	import Button, { Label } from "@smui/button";
+	import DataTable, { Head, Body, Row, Cell } from "@smui/data-table";
 	import "../../styles/_theme_with_material.scss";
 </script>
 
-<Grid>
-	<Row>
-		<Column>
-			<h1>Building with Svelte Material UI</h1>
-			<p>Built with SvelteKit and Carbon Svelte Components</p>
-		</Column>
-	</Row>
+<LayoutGrid>
+	<LayoutCell span="{12}">
+		<h1>Building with Svelte Material UI</h1>
+		<p>Built with SvelteKit and Carbon Svelte Components</p>
+	</LayoutCell>
 
-	<Row style="margin-bottom: var(--cds-spacing-16)">
-		<Column>
-			<h2 class="h4">Sample buttons</h2>
-			<Row>
-				<Column>
-					<Button on:click="{() => clicked++}" variant="raised">
-						<Label>Raised</Label>
-					</Button>
-					<Button on:click="{() => clicked++}" variant="unelevated">
-						<Label>Unelevated</Label>
-					</Button>
-					<Button on:click="{() => clicked++}" variant="outlined">
-						<Label>Outlined</Label>
-					</Button>
-					<Button color="secondary" on:click="{() => clicked++}" variant="raised">
-						<Label>Raised</Label>
-					</Button>
-					<Button color="secondary" on:click="{() => clicked++}" variant="unelevated">
-						<Label>Unelevated</Label>
-					</Button>
-					<Button color="secondary" on:click="{() => clicked++}" variant="outlined">
-						<Label>Outlined</Label>
-					</Button>
-				</Column>
-			</Row>
-		</Column>
-	</Row>
-</Grid>
+	<LayoutCell span="{12}">
+		<h2 class="h4">Sample buttons</h2>
+		<Button variant="raised">
+			<Label>Raised</Label>
+		</Button>
+		<Button variant="unelevated">
+			<Label>Unelevated</Label>
+		</Button>
+		<Button variant="outlined">
+			<Label>Outlined</Label>
+		</Button>
+		<Button color="secondary" variant="raised">
+			<Label>Raised</Label>
+		</Button>
+		<Button color="secondary" variant="unelevated">
+			<Label>Unelevated</Label>
+		</Button>
+		<Button color="secondary" variant="outlined">
+			<Label>Outlined</Label>
+		</Button>
+	</LayoutCell>
+
+	<LayoutCell span="{12}">
+		<h2 class="h4">Sample DataTable</h2>
+		<DataTable table$aria-label="People list" style="max-width: 100%;">
+			<Head>
+				<Row>
+					<Cell>Name</Cell>
+					<Cell>Favorite Color</Cell>
+					<Cell numeric>Favorite Number</Cell>
+				</Row>
+			</Head>
+			<Body>
+				<Row>
+					<Cell>Steve</Cell>
+					<Cell>Red</Cell>
+					<Cell numeric>45</Cell>
+				</Row>
+				<Row>
+					<Cell>Sharon</Cell>
+					<Cell>Purple</Cell>
+					<Cell numeric>5</Cell>
+				</Row>
+				<Row>
+					<Cell>Rodney</Cell>
+					<Cell>Orange</Cell>
+					<Cell numeric>32</Cell>
+				</Row>
+				<Row>
+					<Cell>Mack</Cell>
+					<Cell>Blue</Cell>
+					<Cell numeric>12</Cell>
+				</Row>
+			</Body>
+		</DataTable>
+	</LayoutCell>
+</LayoutGrid>
