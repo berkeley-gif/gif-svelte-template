b1c41fbd9e71820387e5d5f1d17617f45b2a417d
Shruti Mukhtyar <mukhtyar@berkeley.edu>
2023-04-21 09:08:20 -0700

    add project desc

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,12 @@
 <h1>Welcome to the GIF SvelteKit Template</h1>
 
+<p>
+	This repo serves as a reference for setting up new GIF projects using
+	[SvelteKit](https://kit.svelte.dev/) with 2 different Svelte component libraries.
+</p>
+
+<strong> Please do a hard refresh on each page to see the correct styles.</strong>
+
 <ul>
 	<li>
 		<a href="/carbon">Using & Styling with Carbon Components Svelte</a>
